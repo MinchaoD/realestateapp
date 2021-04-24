46cd4e5d0c303b7afe18b5f93d5aa875ab76669e
chaohg <chaohg@hotmail.com>
2021-04-23 22:15:56 -0700

    add houselistcomponent

--- src/components/HouseListComponent.js
+++ src/components/HouseListComponent.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
+import { Card, CardImg, CardGroup, CardTitle, CardText, CardBody, CardFooter, CardDeck} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderHouseList({house}) {
-    return (
-        <Card>
-            <CardImg width="100%" src={house.image} alt={house.location} />
 
-        </Card>
+
+
+function RenderHouseList({houseitem}) {
+    return (
+
+            <div className ="row justify-content-center">
+                <div className = "col ">
+                    <CardDeck>
+                        <Card >
+                            <CardImg  height="400" src={houseitem.image} alt={houseitem.location} />
+                            <CardBody>
+                                <CardTitle>$ {houseitem.price}</CardTitle>
+                                <CardText>
+                                    <div className = "row">
+                                        <div className="col col-md-3 m-1" >
+                                            {houseitem.beds} beds
+                                        </div>
+                                        <div className="col col-md-3 m-1" >
+                                            {houseitem.baths} baths
+                                        </div>
+                                        <div className="col col-md-4 m-1" >
+                                            {houseitem.sqft} Sq.Ft.
+                                        </div>
+                                    </div>
+                                    <div className = "row">
+                                        <div className = "col">
+                                            {houseitem.location}
+                                        </div>
+                                    </div>
+                                </CardText>
+                            </CardBody>
+                            <CardFooter>
+                                <small className="text-muted">Listing provided by {houseitem.listing}</small>
+                            </CardFooter>
+                        </Card>
+                    </CardDeck>
+                </div>
+            </div>
     )
 }
 
@@ -15,17 +48,26 @@ function HouseList (props) {
 
     const houselist = props.houseinfo.map(house => {
         return (
-            <div key = {house.id} className = "col-md-5 m-1">
-                <RenderHouseList house={house} />
+            <div key = {house.id} className = "col-md-6 m-3 mx-auto">
+                <RenderHouseList houseitem={house} />
             </div>
         )
     })
 
         return (
-            <div>
-                {houselist}
-                
+            <div className = "container">
+                <div className = "row">
+                    <h2>
+                        Hot Homes Listing
+                    </h2>
+                </div>        
+                <div className="row">
+ 
+                    {houselist}
+                    
+                </div>
             </div>
+        
         )
     }
 
--- src/shared/houseinfo.js
+++ src/shared/houseinfo.js
@@ -7,6 +7,7 @@ export const HOUSEINFO = [
         beds: 2,
         baths: 2 ,
         price: "849,000",
+        listing: "nwmls",
         links: "https://www.redfin.com/WA/Seattle/588-Bell-St-98121/unit-506/home/55385266"
     },
     {
@@ -17,6 +18,7 @@ export const HOUSEINFO = [
         beds: 2,
         baths: 2,
         price: "879,000",
+        listing: "nwmls",
         links: "https://www.redfin.com/WA/Bellevue/1085-103rd-Ave-NE-98004/unit-220/home/168852312"
     },
     {
@@ -27,6 +29,7 @@ export const HOUSEINFO = [
         beds: 4,
         baths: 5,
         price: "3,688,000",
+        listing: "nwmls",
         links: "https://www.redfin.com/WA/Kirkland/10426-NE-43rd-St-98033/home/12319152"
     },
     {
@@ -37,6 +40,7 @@ export const HOUSEINFO = [
         beds: 5,
         baths: 2.5,
         price: "2,090,000",
+        listing: "nwmls",
         links: "https://www.redfin.com/WA/Seattle/711-37th-Ave-98122/home/141498"
     },
     {
@@ -47,6 +51,7 @@ export const HOUSEINFO = [
         beds: 5,
         baths: 4.5,
         price: "4,500,000",
+        listing: "nwmls",
         links: "https://www.redfin.com/WA/Bellevue/10211-NE-24th-St-98004/home/506309"
     },
     {
@@ -57,6 +62,7 @@ export const HOUSEINFO = [
         beds: 4,
         baths: 3.5,
         price: "1,599,950",
+        listing: "nwmls",
         links: "https://www.redfin.com/WA/Bothell/8823-NE-200th-Pl-98011/home/174864686"
     }
     
