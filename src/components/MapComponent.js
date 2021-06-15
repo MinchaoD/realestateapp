482b2859344eea68d14e75a6fde7aad4c9a516b2
Minchao Duclos <chaohg@hotmail.com>
2021-06-15 09:53:20 -0700

    map still working on

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,45 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { GoogleMap, withScriptjs, Marker, withGoogleMap } from 'react-google-maps';
 import Geocode from 'react-geocode';
 
-let houseLat = ''
-let houseLng = ''
-Geocode.setApiKey('AIzaSyBnNrwpsObb8AcBsyU2nUCKL3CZpSlCgK8');
-Geocode.setLanguage('en');
-Geocode.setRegion('us');
-Geocode.setLocationType('ROOFTOP');
-Geocode.enableDebug();
-Geocode.fromAddress("7425 NE 200th Street, Kenmore, WA98028, USA").then(
-  (response) => {
-    const { lat, lng } = response.results[0].geometry.location;
-    houseLat = lat
-    houseLng = lng
-  },
-  (error) => {
-    console.error(error);
-  }
-);
+class showMap extends Component {
+  constructor(props) {
+    super(props);
+   
+    this.state = {
+      houseLat: '',
+      houseLng: ''
+    };
+    }
+    
 
-console.log(houseLat, houseLng)
+    convertAddress() {
+      Geocode.setApiKey('AIzaSyBnNrwpsObb8AcBsyU2nUCKL3CZpSlCgK8');
+      Geocode.setLanguage('en');
+      Geocode.setRegion('US');
+      Geocode.setLocationType('ROOFTOP');
+      Geocode.enableDebug();
+      Geocode.fromAddress("10426 NE 43rd St, Kirkland, WA 98033, USA").then(
+        (response) => {
+          const { lat, lng } = response.results[0].geometry.location;
+          this.setState({
+            houseLat: lat,
+            houseLng: lng,
+        });
+        
+        },
+        (error) => {
+          console.error(error);
+        }
+      )
+      return
+    }
 
-function showMap() {
-  return (
-    <GoogleMap
-      defaultZoom={15}
-      defaultCenter={{lat: houseLat , lng: houseLng }}>
-      <Marker
-          
-          position={{lat:houseLat , lng: houseLng}} />
-    </GoogleMap>
+    componentDidMount() {this.convertAddress()}; 
 
-  )
-}
+render(){
+  console.log("aabb " +this.state.houseLat)
+  console.log("aabb " +this.state.houseLng)
+  const houseLatNum = Number(`${this.state.houseLat}`)
+  const houseLngNum = Number(`${this.state.houseLng}`)
+  console.log("aabb " +houseLatNum)
+  console.log("aabb " +houseLngNum)
+    return (
+      
+      <GoogleMap
+        defaultZoom={15}
+        defaultCenter={{lat: houseLatNum, lng: houseLngNum}}>
+        <Marker
+            position={{lat: houseLatNum, lng: houseLngNum}} />
+      </GoogleMap>
+   )
+}}
 
 const WrappedMap = withScriptjs(withGoogleMap(showMap));
 
 
 function Map() {
-
 
   return (
     <div style = {{ width: '50vw', height: '50vh'}}>
@@ -53,33 +72,4 @@ function Map() {
 }
 
 export default Map
-
-
-
-// import React, {Component} from 'react';
-// import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-
-
-// class RenderMap extends Component {
-//   render(){
-//       const mapStyles = {
-//           width: 300,
-//           height: 300,
-//         };
-//   return (
-//     <Map
-//       google={this.props.google}
-//       zoom={15}
-//       style={mapStyles}
-//       initialCenter={{ lat: 47.444, lng: -122.176}}
-//     >
-//       <Marker position={{ lat: 48.00, lng: -122.00}} />
-//     </Map>
-//   );
-// }}
-
-
-//   export default GoogleApiWrapper({
-//     apiKey: 'AIzaSyBnNrwpsObb8AcBsyU2nUCKL3CZpSlCgK8'
-//   })(RenderMap);
 
